2e194d2f06ca17b7f9e2a37c6c68ea20ee1011c0
AhmetC <95963297+LastC0der@users.noreply.github.com>
2023-01-10 17:48:10 +0300

    Add files via upload

--- en/qing.ts
+++ en/qing.ts
@@ -4593,6 +4593,7 @@ Check path and filename.</source>
         <source>%n file(s)</source>
         <translation type="unfinished">
             <numerusform></numerusform>
+            <numerusform></numerusform>			
         </translation>
     </message>
     <message>
@@ -7064,6 +7065,7 @@ Please verify the correct file name was given.</source>
         <source>%1 takes at most %n argument(s). %2 is therefore invalid.</source>
         <translation type="unfinished">
             <numerusform></numerusform>
+            <numerusform></numerusform>			
         </translation>
     </message>
     <message>
@@ -7138,6 +7140,7 @@ Please verify the correct file name was given.</source>
         <source>%1 requires at least %n argument(s). %2 is therefore invalid.</source>
         <translation type="unfinished">
             <numerusform></numerusform>
+            <numerusform></numerusform>			
         </translation>
     </message>
     <message>
